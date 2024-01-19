59e37c7e840aa54715dbf36c4dc62bae5bbdd5e8
Matthew Clark <43628254+mon5termatt@users.noreply.github.com>
2024-01-19 01:27:32 -0600

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,40 +1,9 @@
-@ECHO OFF
-REM BFCPEOPTIONSTART
-REM Advanced BAT to EXE Converter www.BatToExeConverter.com
-REM BFCPEEXE=F:\Videos\converter\Downloader And Trimmer.exe
-REM BFCPEICON=F:\Videos\converter\logo0.ico
-REM BFCPEICONINDEX=-1
-REM BFCPEEMBEDDISPLAY=0
-REM BFCPEEMBEDDELETE=1
-REM BFCPEADMINEXE=0
-REM BFCPEINVISEXE=0
-REM BFCPEVERINCLUDE=1
-REM BFCPEVERVERSION=1.9.0.0
-REM BFCPEVERPRODUCT=Wallpaper Maker - Downloader and Trimmer
-REM BFCPEVERDESC=Downloads youtube videos and trims them.
-REM BFCPEVERCOMPANY=https://mon5termatt.club
-REM BFCPEVERCOPYRIGHT=What?
-REM BFCPEOPTIONEND
-@ECHO ON
-@echo off
-
 echo.Welcome to my program, please use it responsibly.
 echo.I do not support copyright infringment so be sure to
 echo.only use this program on material you own ;) 
 echo.
 echo.This Program Download and Converts MKV/WEBM to MP4
 echo.While also trimming it for use in Wallpaper Engine
-set ver=1900
-powershell -c "Invoke-WebRequest -Uri 'http://cdn.mon5termatt.club/files/Programs/Wallpaper_Maker/v%ver%.txt' -OutFile './motd.txt'"
-powershell -c "Invoke-WebRequest -Uri 'http://cdn.mon5termatt.club/files/Programs/Wallpaper_Maker/currentversion.txt' -OutFile './ver.txt'"
-set /p remver= < ver.txt
-IF EXIST "update.bat" del "update.bat"
-if "%ver%" == "%remver%" (goto prestart) else (goto updateprogram)
-:prestart
-echo.------------------------------------------------------------------------
-type motd.txt
-del motd.txt
-del ver.txt
 echo.
 pause
 :start
@@ -43,8 +12,7 @@ echo.
 Echo.    [1] Download a Video From Youtube
 Echo.    [2] Convert the Video to MP4
 Echo.    [3] Convert And Trim the Video
-Echo.    [4] Update Requirements
-Echo.    [5] Exit
+Echo.    [4] Exit
 echo.
 echo.
 Set /P _num=Enter Your Choice [1-5] : || Set _num=NothingChosen
@@ -52,8 +20,7 @@ If "%_num%"=="NothingChosen" goto error
 If /i "%_num%"=="1" goto download
 If /i "%_num%"=="2" set edit=no && cls && goto trim
 If /i "%_num%"=="3" set edit=yes && cls && goto trim
-If /i "%_num%"=="4" goto update
-If /i "%_num%"=="5" exit
+If /i "%_num%"=="4" exit
 :error
 echo.Invalid option
 timeout 3 > nul
@@ -123,41 +90,3 @@ cls
 echo.Video Trimming has completed, Please hit any key to exit...
 pause > nul
 exit
-:update
-cls
-echo.     What do you want to update:
-echo.
-Echo.    [1] YoutubeDL
-Echo.    [2] HandBrake
-Echo.    [3] FFMPEG
-echo.    [4] Exit
-choice /C:1234 /N /M ".Enter Your Choice [1,2,3,4] : "
-if errorlevel 4 exit
-if errorlevel 3 goto:3
-if errorlevel 2 goto:2
-if errorlevel 1 goto:1
-:2
-echo.Download the latest version of Handbrake
-echo.Then drag it into the folder and overwrite the old file 
-start http://url.mon5termatt.club/handbrake
-pause
-goto:update
-:1
-echo.Download the latest version of Youtube DL
-echo.Then drag it into the folder and overwrite the old file 
-start http://url.mon5termatt.club/youtubedl
-pause
-goto:update
-:3
-echo.Step 1: Download the latest version of FFMPEG
-echo.Step 2: Navigate to the bin folder
-echo.Step 3: Extract the bin folder to here
-start http://url.mon5termatt.club/ffmpeg
-pause
-goto:update
-
-
-:updateprogram
-powershell -c "Invoke-WebRequest -Uri 'http://cdn.mon5termatt.club/files/Programs/Wallpaper_Maker/Files/Build.exe' -OutFile './Downloader And Trimmer.exe.new'"
-powershell -c "Invoke-WebRequest -Uri 'http://cdn.mon5termatt.club/files/Programs/Wallpaper_Maker/Files/update.bat' -OutFile './update.bat'"
-start cmd /k CALL update.bat
